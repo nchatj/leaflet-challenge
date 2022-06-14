dfa84d7378c2fc2db5f697f356cd8fcab05d4c12
nchatj <98578803+nchatj@users.noreply.github.com>
2022-06-14 22:34:06 +1000

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,1 @@
-var config = {
-  API_KEY = "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
-}
+var API_KEY = "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
